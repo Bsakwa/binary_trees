bb55ca31dd571b2b26726859661e2de307df2a9d
Bsakwa <sakwabrian23@gmail.com>
2023-03-01 09:08:55 +0000

    Check for a leaf node

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -10,7 +10,8 @@
 
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (node == NULL || node->right == NULL && node->left == NULL)
+	if (node->right == NULL && node->left == NULL)
 		return (1);
+	node = NULL;
 	return (0);
 }
